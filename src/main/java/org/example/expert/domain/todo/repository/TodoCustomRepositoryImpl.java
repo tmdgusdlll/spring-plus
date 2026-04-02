81c6820caf1700df2ba17929141ada4542a78685
tmdgsudl <leesh27955@gmail.com>
2026-04-02 11:51:38 +0900

    Level 2: 8번 완료
    QueryDsl로 변환하여 n+1 방지

---
+++ src/main/java/org/example/expert/domain/todo/repository/TodoCustomRepositoryImpl.java
@@ -0,0 +1,31 @@
+package org.example.expert.domain.todo.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.example.expert.domain.todo.entity.Todo;
+
+import java.util.Optional;
+
+import static org.example.expert.domain.todo.entity.QTodo.todo;
+import static org.example.expert.domain.user.entity.QUser.user;
+
+public class TodoCustomRepositoryImpl implements TodoCustomRepository{
+
+    private final JPAQueryFactory queryFactory;
+
+    public TodoCustomRepositoryImpl(EntityManager em) {
+        queryFactory = new JPAQueryFactory(em);
+    }
+
+    @Override
+    public Optional<Todo> findByIdWithUser(Long todoId) {
+        return Optional.ofNullable(
+                queryFactory
+                        .selectFrom(todo)
+                        .leftJoin(todo.user, user)
+                        .fetchJoin()
+                        .where(todo.id.eq(todoId))
+                        .fetchOne()
+        );
+    }
+}
